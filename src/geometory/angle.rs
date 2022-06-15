d54a91190d3fb68b169e62bb194f2ad2650329eb
wcaokaze <wcaokaze@gmail.com>
2022-06-16 01:07:40 +0900

    added Angle

---
+++ src/geometory/angle.rs
@@ -0,0 +1,268 @@
+use std::cmp::Ordering;
+use super::IterableAngleRange;
+use std::fmt::{self, Display, Formatter};
+use std::ops::{
+    Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign
+};
+
+/// Angle.
+///
+/// We must specify a unit to use Angle.
+/// And, to use `deg()`, we must `use AngleLiteral`.
+/// ```
+/// use typed_scad::geometory::{Angle, AngleLiteral};
+/// let angle: Angle = 90.deg();
+/// ```
+///
+/// Basic operators are available for Angle.
+/// ```
+/// # use typed_scad::geometory::AngleLiteral;
+/// assert_eq!(1.deg() + 2.deg(), 3.deg());
+/// assert_eq!(1.deg() * 2, 2.deg());
+/// assert_eq!(2.deg() / 2, 1.deg());
+/// assert_eq!(4.deg() / 2.deg(), 2.0);
+/// ```
+///
+/// ### Note
+/// Angle implements PartialEq and PartialOrd.
+/// They allows float-point arithmetic errors.
+/// ```
+/// # use std::f64::consts::PI;
+/// # use typed_scad::geometory::AngleLiteral;
+/// assert_ne!(
+///     PI + 1.0_f64.to_radians(),
+///     181.0_f64.to_radians()
+/// );
+///
+/// assert_eq!(
+///     PI.rad() + 1.deg(),
+///     181.deg()
+/// );
+/// ```
+///
+/// And also note that they don't consider circling.
+/// ```
+/// # use typed_scad::geometory::AngleLiteral;
+/// assert_ne!(0.deg(), 360.deg());
+/// ```
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Angle(f64);
+
+impl Angle {
+    /// Converts this angle to a f64 value as radian
+    pub fn to_radian(&self) -> f64 {
+        self.0
+    }
+
+    /// Converts this angle to a f64 value as degree
+    pub fn to_degree(&self) -> f64 {
+        self.0.to_degrees()
+    }
+
+    /// Prepare to iterate [Angle]s in the specified range.
+    /// And [step][IterableAngleRange::step] returns an [Iterator] for Angle.
+    ///
+    /// ```
+    /// # use typed_scad::geometory::{Angle, AngleLiteral, IterableAngleRange};
+    /// let iter = Angle::iterate(0.deg()..=3.deg()).step(1.deg());
+    /// assert_eq!(iter.collect::<Vec<_>>(), vec![0.deg(), 1.deg(), 2.deg(), 3.deg()]);
+    /// ```
+    ///
+    /// Negative steps are also available.
+    /// ```
+    /// # use typed_scad::geometory::{Angle, AngleLiteral, IterableAngleRange};
+    /// let iter = Angle::iterate(3.deg()..=0.deg()).step(-1.deg());
+    /// assert_eq!(iter.collect::<Vec<_>>(), vec![3.deg(), 2.deg(), 1.deg(), 0.deg()]);
+    /// ```
+    pub fn iterate<R>(angle_range: R) -> R where R: IterableAngleRange {
+        angle_range
+    }
+}
+
+impl Display for Angle {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_fmt(format_args!("{}°", self.0.to_degrees()))
+    }
+}
+
+const D: f64 = 1e-10;
+
+impl PartialOrd for Angle {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self.0 < other.0 - D, self.0 > other.0 + D) {
+            (false, false) => None,
+            (false,  true) => Some(Ordering::Greater),
+            ( true, false) => Some(Ordering::Less),
+            ( true,  true) => Some(Ordering::Equal)
+        }
+    }
+}
+
+impl PartialEq for Angle {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 > other.0 - D && self.0 < other.0 + D
+    }
+}
+
+impl Add for Angle {
+    type Output = Angle;
+    fn add(self, rhs: Angle) -> Angle { Angle(self.0 + rhs.0) }
+}
+
+impl AddAssign for Angle {
+    fn add_assign(&mut self, rhs: Angle) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub for Angle {
+    type Output = Angle;
+    fn sub(self, rhs: Angle) -> Angle { Angle(self.0 - rhs.0) }
+}
+
+impl SubAssign for Angle {
+    fn sub_assign(&mut self, rhs: Angle) {
+        *self = *self - rhs;
+    }
+}
+
+impl<Rhs> Mul<Rhs> for Angle where Rhs: Into<f64> {
+    type Output = Angle;
+    fn mul(self, rhs: Rhs) -> Angle { Angle(self.0 * rhs.into()) }
+}
+
+impl<Rhs> MulAssign<Rhs> for Angle where Rhs: Into<f64> {
+    fn mul_assign(&mut self, rhs: Rhs) {
+        *self = *self * rhs;
+    }
+}
+
+impl<Rhs> Div<Rhs> for Angle where Rhs: Into<f64> {
+    type Output = Angle;
+    fn div(self, rhs: Rhs) -> Angle { Angle(self.0 / rhs.into()) }
+}
+
+impl<Rhs> DivAssign<Rhs> for Angle where Rhs: Into<f64> {
+    fn div_assign(&mut self, rhs: Rhs) {
+        *self = *self / rhs;
+    }
+}
+
+impl Div for Angle {
+    type Output = f64;
+    fn div(self, rhs: Angle) -> f64 { self.0 / rhs.0 }
+}
+
+impl Neg for Angle {
+    type Output = Angle;
+    fn neg(self) -> Angle { Angle(-self.0) }
+}
+
+/// Type that can make [Angle] with `deg()` postfix.
+///
+/// Rust's primitive numbers are AngleLiteral.
+/// ```
+/// # use typed_scad::geometory::AngleLiteral;
+/// 1.deg();
+/// 2.0.deg();
+/// ```
+pub trait AngleLiteral {
+    fn deg(self) -> Angle;
+    fn rad(self) -> Angle;
+}
+
+impl<T> AngleLiteral for T where T: Into<f64> {
+    fn deg(self) -> Angle {
+        Angle(self.into().to_radians())
+    }
+
+    fn rad(self) -> Angle {
+        Angle(self.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Angle, AngleLiteral};
+    use std::f64::consts::PI;
+
+    #[test]
+    fn eq() {
+        assert_eq!(Angle(0.42), Angle(0.42));
+        assert_ne!(Angle(0.42), Angle(0.43));
+
+        assert_ne!(      0.42,        0.42 + 1e-12);
+        assert_eq!(Angle(0.42), Angle(0.42 + 1e-12));
+
+        assert_ne!(Angle(0.42), Angle(0.42 + 2.0 * PI));
+    }
+
+    #[test]
+    fn display() {
+        assert_eq!(
+            format!("{}", Angle(PI)),
+            "180°".to_string()
+        );
+    }
+
+    #[test]
+    fn angle_literal() {
+        assert_eq!(2.rad(), Angle(2.0));
+        assert_eq!(180.deg(), Angle(PI));
+        assert_eq!(0.42.rad(), Angle(0.42));
+        assert_eq!(180.0.deg(), Angle(PI));
+    }
+
+    #[test]
+    fn to_radian() {
+        assert_eq!(Angle(0.42).to_radian(), 0.42);
+    }
+
+    #[test]
+    fn to_degree() {
+        assert_eq!(Angle(PI).to_degree(), 180.0);
+    }
+
+    #[test]
+    fn operators() {
+        assert_eq!(Angle( 0.42) + Angle( 0.15), Angle(0.57));
+        assert_eq!(Angle( 0.42) + Angle(-0.15), Angle(0.27));
+        assert_eq!(Angle(-0.42) + Angle( 0.15), Angle(-0.27));
+        assert_eq!(Angle(-0.42) + Angle(-0.15), Angle(-0.57));
+
+        assert_eq!(Angle( 0.42) - Angle( 0.15), Angle(0.27));
+        assert_eq!(Angle( 0.42) - Angle(-0.15), Angle(0.57));
+        assert_eq!(Angle(-0.42) - Angle( 0.15), Angle(-0.57));
+        assert_eq!(Angle(-0.42) - Angle(-0.15), Angle(-0.27));
+
+        assert_eq!(Angle( 0.42) *  2, Angle( 0.84));
+        assert_eq!(Angle( 0.42) * -2, Angle(-0.84));
+        assert_eq!(Angle(-0.42) *  2, Angle(-0.84));
+        assert_eq!(Angle(-0.42) * -2, Angle( 0.84));
+        assert_eq!(Angle( 0.42) *  1.5, Angle( 0.63));
+        assert_eq!(Angle( 0.42) * -1.5, Angle(-0.63));
+        assert_eq!(Angle(-0.42) *  1.5, Angle(-0.63));
+        assert_eq!(Angle(-0.42) * -1.5, Angle( 0.63));
+
+        assert_eq!(Angle( 0.42) /  2, Angle( 0.21));
+        assert_eq!(Angle( 0.42) / -2, Angle(-0.21));
+        assert_eq!(Angle(-0.42) /  2, Angle(-0.21));
+        assert_eq!(Angle(-0.42) / -2, Angle( 0.21));
+        assert_eq!(Angle( 0.42) /  1.5, Angle( 0.28));
+        assert_eq!(Angle( 0.42) / -1.5, Angle(-0.28));
+        assert_eq!(Angle(-0.42) /  1.5, Angle(-0.28));
+        assert_eq!(Angle(-0.42) / -1.5, Angle( 0.28));
+
+        // 0.42 causes a float-point arithmetic error. We should use 2⁻ⁿ here
+        assert_eq!(Angle( 0.25) / Angle( 0.5),  0.5);
+        assert_eq!(Angle( 0.25) / Angle(-0.5), -0.5);
+        assert_eq!(Angle(-0.25) / Angle( 0.5), -0.5);
+        assert_eq!(Angle(-0.25) / Angle(-0.5),  0.5);
+
+        assert_eq!(-Angle(0.42), Angle(-0.42));
+
+        assert!(Angle(0.42) > Angle(0.41));
+        assert!(Angle(0.41) < Angle(0.42));
+        assert!(Angle(0.42) < Angle(0.42 + 2.0 * PI));
+    }
+}
---
+++ src/geometory/angle_iterator.rs
@@ -0,0 +1,155 @@
+use super::{Angle, AngleLiteral};
+use std::ops::{Bound, Range, RangeBounds, RangeFrom, RangeInclusive};
+
+/// An [Iterator] for [Angle].
+pub struct AngleIterator {
+    next: Option<Angle>,
+    step: Angle,
+    end_bound: Bound<Angle>
+}
+
+impl AngleIterator {
+    fn new(
+        start_value: Angle, step: Angle, end_bound: Bound<Angle>
+    ) -> AngleIterator {
+        let is_iterable = Self::is_available_value(start_value, end_bound, step);
+
+        AngleIterator {
+            next: if is_iterable { Some(start_value) } else { None },
+            step,
+            end_bound
+        }
+    }
+
+    fn calc_next(&self, current: Angle) -> Option<Angle> {
+        let next = current + self.step;
+        let has_next = Self::is_available_value(next, self.end_bound, self.step);
+        if has_next { Some(next) } else { None }
+    }
+
+    fn is_available_value(
+        value: Angle,
+        end_bound: Bound<Angle>,
+        step: Angle
+    ) -> bool {
+        match end_bound {
+            Bound::Included(end) => Self::is_not_ended(value, end + step / 16, step),
+            Bound::Excluded(end) => Self::is_not_ended(value, end,             step),
+            Bound::Unbounded => true
+        }
+    }
+
+    fn is_not_ended(value: Angle, end_value: Angle, step: Angle) -> bool {
+        if step > 0.rad() {
+            value < end_value
+        } else {
+            value > end_value
+        }
+    }
+}
+
+impl Iterator for AngleIterator {
+    type Item = Angle;
+
+    fn next(&mut self) -> Option<Angle> {
+        let next = self.next?;
+        self.next = self.calc_next(next);
+        Some(next)
+    }
+}
+
+/// Range that can iterate with [Angle::iterate].
+///
+/// The follow 3 range types are iterable.
+/// - [Range] `0.deg()..3.deg()`
+/// - [RangeInclusive] `0.deg()..=3.deg()`
+/// - [RangeFrom] `0.deg()..`
+pub trait IterableAngleRange: RangeBounds<Angle> {
+    fn start_value(&self) -> Angle;
+
+    fn step(self, step: Angle) -> AngleIterator where Self: Sized {
+        AngleIterator::new(
+            self.start_value(),
+            step,
+            self.end_bound().cloned()
+        )
+    }
+}
+
+impl IterableAngleRange for Range<Angle> {
+    fn start_value(&self) -> Angle {
+        self.start
+    }
+}
+
+impl IterableAngleRange for RangeInclusive<Angle> {
+    fn start_value(&self) -> Angle {
+        *self.start()
+    }
+}
+
+impl IterableAngleRange for RangeFrom<Angle> {
+    fn start_value(&self) -> Angle {
+        self.start
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::{Angle, AngleLiteral};
+    use super::IterableAngleRange;
+
+    #[test]
+    fn iterate() {
+        let expected = vec![42.deg(), 43.5.deg(), 45.deg()];
+        let actual: Vec<_> = Angle::iterate(42.deg()..=45.deg()).step(1.5.deg())
+            .collect();
+        assert_eq!(actual, expected);
+
+        let expected = vec![42.deg(), 43.5.deg()];
+        let actual: Vec<_> = Angle::iterate(42.deg()..45.deg()).step(1.5.deg())
+            .collect();
+        assert_eq!(actual, expected);
+
+        let expected = vec![42.deg(), 43.5.deg(), 45.deg()];
+        let actual: Vec<_> = Angle::iterate(42.deg()..).step(1.5.deg())
+            .take(3)
+            .collect();
+        assert_eq!(actual, expected);
+
+        let actual: Vec<_> = Angle::iterate(45.deg()..=42.deg()).step(1.5.deg())
+            .collect();
+        assert_eq!(actual, vec![]);
+
+        let actual: Vec<_> = Angle::iterate(45.deg()..42.deg()).step(1.5.deg())
+            .collect();
+        assert_eq!(actual, vec![]);
+    }
+
+    #[test]
+    fn iterate_down() {
+        let expected = vec![45.deg(), 43.5.deg(), 42.deg()];
+        let actual: Vec<_> = Angle::iterate(45.deg()..=42.deg()).step(-1.5.deg())
+            .collect();
+        assert_eq!(actual, expected);
+
+        let expected = vec![45.deg(), 43.5.deg()];
+        let actual: Vec<_> = Angle::iterate(45.deg()..42.deg()).step(-1.5.deg())
+            .collect();
+        assert_eq!(actual, expected);
+
+        let expected = vec![45.deg(), 43.5.deg(), 42.deg()];
+        let actual: Vec<_> = Angle::iterate(45.deg()..).step(-1.5.deg())
+            .take(3)
+            .collect();
+        assert_eq!(actual, expected);
+
+        let actual: Vec<_> = Angle::iterate(42.deg()..=45.deg()).step(-1.5.deg())
+            .collect();
+        assert_eq!(actual, vec![]);
+
+        let actual: Vec<_> = Angle::iterate(42.deg()..45.deg()).step(-1.5.deg())
+            .collect();
+        assert_eq!(actual, vec![]);
+    }
+}
