28dc2999b225d828ea546717ab94e847e7d662ff
wcaokaze <wcaokaze@gmail.com>
2022-07-24 05:18:04 +0900

    added Transform::translated, rotated

--- src/transform/transform.rs
+++ src/transform/transform.rs
@@ -1,12 +1,24 @@
 use crate::geometry::{Angle, Line, Size, Vector};
 
-pub trait Transform {
-   fn translate(&mut self, offset: &Vector);
+pub trait Transform: Sized {
+   fn translated(&self, offset: &Vector) -> Self;
 
-   fn translate_toward(&mut self, direction: &Vector, distance: Size) {
-      let v = *direction * (distance / direction.norm());
-      self.translate(&v);
+   fn translate(&mut self, offset: &Vector) {
+      *self = self.translated(offset);
    }
 
-   fn rotate(&mut self, axis: &Line, angle: Angle);
+   fn translated_toward(&self, direction: &Vector, distance: Size) -> Self {
+      let v = *direction * (distance / direction.norm());
+      self.translated(&v)
+   }
+
+   fn translate_toward(&mut self, direction: &Vector, distance: Size) {
+      *self = self.translated_toward(direction, distance);
+   }
+
+   fn rotated(&self, axis: &Line, angle: Angle) -> Self;
+
+   fn rotate(&mut self, axis: &Line, angle: Angle) {
+      *self = self.rotated(axis, angle);
+   }
 }
