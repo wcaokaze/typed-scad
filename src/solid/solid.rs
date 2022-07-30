eda98f969db6946b273c50ed72b2da864091eac9
wcaokaze <wcaokaze@gmail.com>
2022-07-31 08:50:55 +0900

    added Solid::build

--- src/solid/solid.rs
+++ src/solid/solid.rs
@@ -10,4 +10,39 @@ pub trait Solid {
       write_stl(output, &stl_solid)?;
       Ok(())
    }
+
+   fn build(builder: impl FnOnce(&mut Self) -> ()) -> Self
+      where Self: Default
+   {
+      let mut solid = Self::default();
+      builder(&mut solid);
+      solid
+   }
+}
+
+#[cfg(test)]
+mod test {
+   use super::Solid;
+   use crate::stl::StlSolid;
+
+   #[test]
+   fn build() {
+      struct SolidImpl(i32);
+      impl Default for SolidImpl {
+         fn default() -> SolidImpl {
+            SolidImpl(0)
+         }
+      }
+      impl Solid for SolidImpl {
+         fn generate_stl_solid(&self) -> StlSolid {
+            panic!();
+         }
+      }
+
+      let solid_impl = SolidImpl::build(|s| {
+         s.0 = 42;
+      });
+
+      assert_eq!(solid_impl.0, 42);
+   }
 }
