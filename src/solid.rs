70ece42852a8ed6eae179820e4e8e4b4de9c6d88
wcaokaze <wcaokaze@gmail.com>
2022-08-05 01:39:44 +0900

    added: solid::primitive::Cylinder

--- src/solid.rs
+++ src/solid.rs
@@ -9,6 +9,7 @@ mod solid_parent;
 pub use location::Location;
 pub use location_builder::LocationBuilder;
 pub use primitive::cube::{cube, Cube};
+pub use primitive::cylinder::{cylinder, Cylinder};
 pub use primitive::precision;
 pub use solid::Solid;
 pub use solid_parent::SolidParent;
--- src/solid/primitive.rs
+++ src/solid/primitive.rs
@@ -1,3 +1,4 @@
 
 pub(in crate::solid) mod cube;
+pub(in crate::solid) mod cylinder;
 pub mod precision;
