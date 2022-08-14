456e1be37a59aef24475e6c642ad570f031656c5
wcaokaze <wcaokaze@gmail.com>
2022-08-15 04:58:56 +0900

    added solid::primitive::rotate::Rotate

--- src/solid.rs
+++ src/solid.rs
@@ -11,6 +11,7 @@ pub use location_builder::LocationBuilder;
 pub use primitive::cone::{cone, Cone};
 pub use primitive::cube::{cube, Cube};
 pub use primitive::cylinder::{cylinder, Cylinder};
+pub use primitive::rotate::{rotate, Rotate};
 pub use primitive::sphere::{sphere, Sphere};
 pub use primitive::translate::{translate, Translate};
 pub use primitive::precision;
--- src/solid/primitive.rs
+++ src/solid/primitive.rs
@@ -2,6 +2,7 @@
 pub(in crate::solid) mod cone;
 pub(in crate::solid) mod cube;
 pub(in crate::solid) mod cylinder;
+pub(in crate::solid) mod rotate;
 pub(in crate::solid) mod sphere;
 pub(in crate::solid) mod translate;
 pub mod precision;
