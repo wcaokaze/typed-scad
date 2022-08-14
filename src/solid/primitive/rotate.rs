5ababd1f2ed3628d4cbf7a2c7cc676e4c4328cf1
wcaokaze <wcaokaze@gmail.com>
2022-08-15 07:04:52 +0900

    faster implement for Rotate whose axis is on Point::ORIGIN

--- src/solid/primitive/rotate.rs
+++ src/solid/primitive/rotate.rs
@@ -1,9 +1,10 @@
-use crate::geometry::{Angle, Line};
+use crate::geometry::{Angle, Line, Point, Vector};
 use crate::solid::builder::BuildContext;
 use crate::solid::{Solid, SolidParent};
 use crate::solid::solid_parent::PushBorrowing;
 use crate::stl::StlSolid;
 use crate::transform::Transform;
+use std::mem;
 
 pub struct Rotate {
    pub axis: Line,
@@ -40,9 +41,21 @@ impl Solid for Rotate {
             .collect()
       };
 
-      for f in &mut stl_solid.facets {
-         for v in &mut f.vertexes {
-            v.rotate(&self.axis, self.angle);
+      if self.axis.point() == Point::ORIGIN {
+         let axis = self.axis.vector();
+         for f in &mut stl_solid.facets {
+            for v in &mut f.vertexes {
+               unsafe {
+                  mem::transmute::<&mut Point, &mut Vector>(v)
+                     .rotate(axis, self.angle);
+               }
+            }
+         }
+      } else {
+         for f in &mut stl_solid.facets {
+            for v in &mut f.vertexes {
+               v.rotate(&self.axis, self.angle);
+            }
          }
       }
 
@@ -59,7 +72,7 @@ impl SolidParent for Rotate {
 #[cfg(test)]
 mod tests {
    use super::rotate;
-   use crate::geometry::{AngleLiteral, Line, Point, SizeLiteral};
+   use crate::geometry::{AngleLiteral, Line, Point, SizeLiteral, Vector};
    use crate::solid::Solid;
    use crate::stl::{Facet, StlSolid};
 
@@ -94,6 +107,26 @@ mod tests {
          Point::new(-1.mm(), 0.mm(), 2.mm()),
          Point::new(-4.mm(), 3.mm(), 5.mm()),
          Point::new(-7.mm(), 6.mm(), 8.mm())
+      ];
+
+      assert_eq!(expected, actual);
+
+      let axis = Line::new(
+         &Point::new(1.mm(), 1.mm(), 0.mm()),
+         &Vector::Z_UNIT_VECTOR
+      );
+      let r = rotate(axis, 90.deg(), |mut c| {
+         c <<= Child;
+      });
+      let s = r.generate_stl_solid();
+
+      let actual: Vec<_> = s.facets.iter()
+         .flat_map(|f| f.vertexes)
+         .collect();
+      let expected = vec![
+         Point::new( 1.mm(), 0.mm(), 2.mm()),
+         Point::new(-2.mm(), 3.mm(), 5.mm()),
+         Point::new(-5.mm(), 6.mm(), 8.mm())
       ];
 
       assert_eq!(expected, actual);
